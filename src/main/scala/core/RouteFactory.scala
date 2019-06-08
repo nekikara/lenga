4a7b8432eabad7ee0f7cf8a2040dc59113111f47
nekikara <nekikara@gmail.com>
2019-06-08 17:25:54 +0200

    Easy DIP for routing

--- src/main/scala/core/RouteFactory.scala
+++ src/main/scala/core/RouteFactory.scala
@@ -4,11 +4,11 @@ import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
 
 object RouteFactory {
-  def mailRoutes(routesData: Array[String]): Route = {
+  def mailRoutes(routesData: Array[String])(op: String => Route): Route = {
     val routes = routesData.map(routeName => {
       val pathChunks = routeName.split("\\/").filter(!_.isEmpty)
       path(separateOnSlashes(pathChunks.mkString("/"))) {
-        complete(routeName)
+        op(routeName)
       }
     })
     routes.reduce((x, y) => x ~ y)
--- src/test/scala/core/RouteFactorySpec.scala
+++ src/test/scala/core/RouteFactorySpec.scala
@@ -2,11 +2,16 @@ package core
 
 import org.scalatest._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.http.scaladsl.server.Directives._
 
 class RouteFactorySpec extends WordSpec with Matchers with ScalatestRouteTest {
   "The RouteFactorySpec" should {
     val routes = Array("/test", "/signup", "/password/reset")
-    val testRoute = RouteFactory.mailRoutes(routes)
+    val testRoute = RouteFactory.mailRoutes(routes) { routeName =>
+      get {
+        complete(routeName)
+      }
+    }
     "return test for GET requests to the test path" in {
       Get("/test") ~> testRoute ~> check {
         responseAs[String] shouldEqual "/test"
